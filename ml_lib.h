1511306863fde0e7f2c0eacb34769702564f15fd
nsimonato8 <55627020+nsimonato8@users.noreply.github.com>
2020-11-26 15:15:23 +0100

    Add files via upload

--- ml_lib.h
+++ ml_lib.h
@@ -5,27 +5,51 @@
 #define ROW 7
 #define COL 7
 
+typedef enum {User, Pc} id_p;
+typedef enum {Soldier, Officer} gr;
+
+typedef struct cella pedina;
+
 /*Definizione del tipo pedina*/
-struct cella{
-    int id_player; /*BIANCHI = 0, NERI = 1*/
+ struct cella{
+    id_p id_player; /*BIANCHI = 0, NERI = 1*/
     int id_pedina;
-    int grado;
+    gr grado; /*SOLDATO = 0, UFFICIALE = 1*/
     pedina *middle;
     pedina *down;
 };
 
-typedef struct cella pedina;
+int isForbiddenCell(int x,int y);
 
-pedina **createPedine();
+pedina ***createPedine();
 
-void destroyPedine(pedina **p);
+void destroyPedine(pedina ***p);
 
-pedina **createMatrix(const pedina r, const pedina c);
+pedina ***createMatrix();
 
-void destroyMatrix(pedina **board)
+void destroyMatrix(pedina ***board);
 
-void setValuesMatrix(pedina **board, pedina **players);
+void setValuesMatrix(pedina ***board, pedina ***players);
 
 void printPedina(pedina *p);
 
-void printMatrix(int **m);
+void printMatrix(pedina ***m);
+
+int catchInput(int *cord);
+
+int move(pedina ***p, int from_x, int from_y, int to_x, int to_y, id_p first, int turn); /*TODO: Aggiungere controllo giocatore legittimo*/
+
+int distance(int from_x, int from_y, int to_x, int to_y);
+
+void capture(pedina ***p, int from_x, int from_y, int to_x, int to_y);
+
+int gradeCheck(pedina ***p, int from_x, int from_y, int to_y);
+
+int isWinner(pedina ***board,int player);
+
+int existMandatory(pedina ***p, int from_x, int from_y, int to_x, int to_y); /*Verifica se esiste una mossa (obbligatoria) diversa che da quella tentata */
+
+/*TODO: DA IMPLEMENTARE*/
+
+void printStatus(int turn);
+
--- ml_main.c
+++ ml_main.c
@@ -1,17 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "ml_lib.h"
 
 int main(){
 	
-	int **m;
-	int r=7, c=7;
+	pedina ***board, ***players;
+	int coordinate[4]; /*Contiene le coordinate di partenza e arrivo di ogni mossa*/
+	int success_move = 1, success_input = 1, turn = 0; /*Verifica che la mossa sia possibile*/
+	id_p first = User;
+
+	/*Inizializzazione campo di gioco*/
+	board = createMatrix();
+	players = createPedine();
 	
-	
-	m=createMatrix(r,c);
-	
-	setValuesMatrix(m,r,c);
-	
-	printMatrix(m,r,c);
+	setValuesMatrix(board,players);
+
+
+
+	/*TODO: Menu
+	 * 1) istruzioni --> printf
+	 * 2) gioca --> selezione modalità --> gioco
+	 * 3) credits --> printf
+	 * 4) exit
+	*/
+
+    /*Gioco 1v1*/
+	do{
+        printMatrix(board); /*Status scacchiera */
+        printStatus(turn); /*Status giocatore + n°turno */
+        do{
+            if(!success_move || !success_input){
+                /*TODO: Schermata di errore di input*/
+            }
+            success_input = catchInput(coordinate);
+            success_move = move(board,coordinate[0],coordinate[1],coordinate[2],coordinate[3],first,turn);
+        }while(!(success_input && success_move));/*Inserimento dati e mossa*/
+        turn++;
+	}while(!((isWinner(board,0))||(isWinner(board,1)))); /*Controllo esistenza vincitore*/
+
+        /*TODO: Schermata vittoria*/
+
+	/*Liberazione memoria*/
+	destroyMatrix(board);
+	destroyPedine(players);
 	
 	return 0;
 }
