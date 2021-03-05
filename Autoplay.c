c591c7958c72ff1b89156544347592f50068bf1b
nsimonato8 <nsimonato8@gmail.com>
2021-03-05 16:17:18 +0100

    Update Autoplay.c
    Progettazione gioco automatico

--- Autoplay.c
+++ Autoplay.c
@@ -1,37 +1,89 @@
 #include<stdio.h>
 #include<ml_lib.c>
 
+#define NUMERO_PEDINE 7
+
 typedef struct s_node t_node;
-typedef struct s_row t_row;
-typedef struct s_mossa mossa;
+
+/*alfa -> [-1,1]*/
 
 struct s_node { //coppia valore,mossa
-	int alfa;
-	point destinazione;
+	int alfa; //indice 
+	point start;
+	point end;
+	pedina *data;
 }
 
-struct s_row{ //coppia pedina,(valore,mossa)
-	pedina *ped;
-	t_node value;
+
+t_node database_mosse[NUMERO_PEDINE]; //situazione
+
+
+/*
+function minimax(nodo, profondità)
+
+    SE nodo è un nodo terminale OPPURE profondità = 0
+        return il valore euristico del nodo 
+		
+    SE l'avversario deve giocare
+        α := +∞
+        PER OGNI figlio di nodo
+            α := min(α, minimax(figlio, profondità-1))
+			
+			
+    ALTRIMENTI dobbiamo giocare noi
+        α := -∞
+        PER OGNI figlio di nodo
+            α := max(α, minimax(figlio, profondità-1))
+			
+			
+    return α
+*/
+t_node minimax(/*pedina **board, */t_node node, int depth, int turn){
+	
+	t_node res;
+	if(!depth){
+		res = valutazione_euristica(node);
+	}else{
+		int alfa = INT_MIN;
+		if(turn%2){
+			
+			/*res = node + (+1,+1)
+			/*if (can_eat || can_move){
+				res = minimax(res,depth-1,turn+1)
+				if(alfa < res.alfa)
+					alfa = res.alfa;
+			}
+			*/
+			
+			/*-1,+1*/
+			if(get_grade(node.data) == Officer){
+				/*+1,-1*/
+				/*-1,-1*/
+			}
+			
+		}else{
+			/**/
+			
+		}
+		
+	}
+	return res;
 }
 
-struct s_mossa{
-	pedina *ped;
-	point destinazione;	
-}
-
-t_row database_mosse[2][14]; //situazione
-
-
-
-t_node minimax(t_node node, int depth){
-	//Vedi https://it.wikipedia.org/wiki/Minimax
-}
-
-pedina select_pedina(id_p player, t_row db[2][14]){
-	/*1) Selezione riga
-	* 2) Selezione max alfa;
-	* 3) Ritorno pedina,mossa
-	*/
+/*
+* 1) Selezione riga
+* 2) Selezione max alfa;
+* 3) Ritorno pedina,mossa (t_node)
+*/
+t_node select_pedina(t_node db[NUMERO_PEDINE]){	
+	int i, max = INT_MIN;
+	t_node res;
+	for(i = 0; i < NUMERO_PEDINE; i++){
+		if(db[i].alfa > max){
+			res = db[i];
+			max = db[i].alfa;
+		}			
+	}
+	return res;
 }
 
